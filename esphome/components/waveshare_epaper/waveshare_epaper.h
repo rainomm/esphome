03d719541bd2856354e2d170fb60c31fdade8e90
Tyler Walters <tyler@guardmedia.com>
2022-09-18 22:42:44 -0400

    init reset added to 7P5InBV2, needed after sleep

--- esphome/components/waveshare_epaper/waveshare_epaper.h
+++ esphome/components/waveshare_epaper/waveshare_epaper.h
@@ -336,6 +336,17 @@ class WaveshareEPaper7P5InBV2 : public WaveshareEPaper {
   int get_width_internal() override;
 
   int get_height_internal() override;
+
+  void reset_() {
+    if (this->reset_pin_ != nullptr) {
+      this->reset_pin_->digital_write(true);
+      delay(200);  // NOLINT
+      this->reset_pin_->digital_write(false);
+      delay(2);
+      this->reset_pin_->digital_write(true);
+      delay(20);
+    }
+  }
 };
 
 class WaveshareEPaper7P5InBC : public WaveshareEPaper {
